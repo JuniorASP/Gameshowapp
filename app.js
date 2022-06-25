bb8313e916113ab22f86c2472ed1b2ac0c7cab05
JuniorASP <102246813+JuniorASP@users.noreply.github.com>
2022-06-25 13:40:56 +0200

    gameshowapp

--- app.js
+++ app.js
@@ -2,8 +2,106 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const btn= document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
+const letter = document.querySelectorAll('.letter')
+const li = document.createElement('li');
 let missedScore = 0;
 
+
+const phrases = [
+    'power'
+    ,'godfather'
+    , 'scarface'
+    , 'dragonball'
+    , 'naruto'
+
+];
+
+btn.addEventListener('click', () => {
+    overlay.style.display ='none';
+    
+    });
+
+    function getrandomphrases(arr){
+    const randomNumber = [Math.floor(Math.random() * getrandomphrases.length)];
+    let phrases = arr[randomNumber]
+    return phrases;
+    };
+
+
+    const randomPhrase = getrandomphrases(phrases);
+    function addPhraseToDisplay(arr){
+        for(let i = 0; i< getrandomphrases.length; i++ ){
+            let letter = arr[i];
+           ul.appendChild('li');
+           li.className = 'letter';
+           li.textContent = letter;
+          
+           addPhraseToDisplay(randomPhrase);
+        
+        }
+    }
+
+    
+
+    function checkLetter(button) {
+        const checkLetter = li;    
+        let match = null;
+            for(let i = 0; i< checkLetter.length; i++){
+    if (addPhraseToDisplay === checkLetter[i].textContent) {
+        checkLetter[i].className += ' show'
+        return match;
+    
+        
+    }
+    
+    }
+    }    
+
+    qwerty.addEventListener('click', (e) =>{
+        if(e.target.tagName === 'BUTTON' && e.target.className != 'chosen'){
+            const button = e.target;
+            button.disabled = true;
+            button.className = "chosen";
+            const check = checkLetter(button);
+        
+            if (check === null) {
+                heartArray[missed].src = 'images/lostHeart.png'
+                button.className = 'wrong'
+                missed++
+        
+            }
+        }
+        
+        });
+        
+        const checkWin = () => {
+            const totalLetters = document.querySelectorAll('.letter')
+            const shownLetters = document.querySelectorAll('.show')
+            const h3 = document.createElement('h3')
+            if (shownLetters.length === totalLetters.length) {
+                removeShowClass()
+                overlay.className = 'win'
+                overlay.style.display = 'flex'
+                startButton.textContent = 'Play Again'
+                overlay.appendChild(h3)
+                h3.textContent = 'You Won!'
+            } else if (missed >= 4) {
+                removeShowClass()
+                overlay.className = 'lose'
+                overlay.style.display = 'flex'
+                startButton.textContent = 'Try Again'
+                overlay.appendChild(h3)
+                h3.textContent = 'You Lost!'
+                showCorrectPhrase()
+            }
+        return checkWin
+        }
+        
+        
+        
+        
+
+/*
 function getrandomphrases(){[ 
     'I have a car'
     ,'I love motors'
@@ -92,3 +190,4 @@ const checkWin = () => {
 
 
 }
+*/
